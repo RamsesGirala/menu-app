4ee0158184ea2cba6a7117505f210f94e1469597
ramsesgiralald <ramsesgiralald@gmail.com>
2024-02-26 21:18:27 -0300

    a

--- src/main/java/colmenatec/menuapp/MenuAppApplication.java
+++ src/main/java/colmenatec/menuapp/MenuAppApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MenuAppApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MenuAppApplication.class, args);
+		System.out.println("Estoy funcionando");
 	}
 
 }
